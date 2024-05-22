1837501a02d42c2c9dc13ddd363fb1e832d587b8
Femey <163730144+Femeyuwu@users.noreply.github.com>
2024-05-22 17:52:36 +1200

    a

---
+++ src/main/java/com/example/addon/modules/TaFlight.java
@@ -0,0 +1,93 @@
+package com.example.addon.modules;
+
+import com.example.addon.AddonTemplate;
+import com.google.common.collect.Streams;
+import meteordevelopment.meteorclient.events.entity.player.PlayerMoveEvent;
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.mixin.ClientPlayerEntityAccessor;
+import meteordevelopment.meteorclient.mixin.PlayerMoveC2SPacketAccessor;
+import meteordevelopment.meteorclient.mixininterface.IVec3d;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.player.PlayerUtils;
+import meteordevelopment.meteorclient.utils.world.TickRate;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShape;
+
+import java.util.stream.Stream;
+
+public class TaFlight extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgAntiKick = settings.createGroup("Anti Kick");
+
+    private final Setting<Double> speed = sgGeneral.add(new DoubleSetting.Builder()
+        .name("speed")
+        .description("Your speed when flying.")
+        .defaultValue(1.65)
+        .min(0.0)
+        .sliderMax(10)
+        .build()
+    );
+
+    private final Setting<Boolean> verticalSpeedMatch = sgGeneral.add(new BoolSetting.Builder()
+        .name("vertical-speed-match")
+        .description("Matches your vertical speed to your horizontal speed, otherwise uses vanilla ratio.")
+        .defaultValue(false)
+        .build()
+    );
+
+    public TaFlight() {
+        super(AddonTemplate.CATEGORY, "TaFlight", "TaFlight speed at 1.65");
+    }
+    
+
+    @EventHandler
+    private void onPlayerMove(PlayerMoveEvent event) {
+
+        fullFlightMove(event, speed.get() * (TickRate.INSTANCE.getTickRate() / 20), verticalSpeedMatch.get());
+
+    }
+
+    private double getDir() {
+        double dir = 0;
+
+        if (Utils.canUpdate()) {
+            dir = mc.player.getYaw() + ((mc.player.forwardSpeed < 0) ? 180 : 0);
+
+            if (mc.player.sidewaysSpeed > 0) {
+                dir += -90F * ((mc.player.forwardSpeed < 0) ? -0.5F : ((mc.player.forwardSpeed > 0) ? 0.5F : 1F));
+            } else if (mc.player.sidewaysSpeed < 0) {
+                dir += 90F * ((mc.player.forwardSpeed < 0) ? -0.5F : ((mc.player.forwardSpeed > 0) ? 0.5F : 1F));
+            }
+        }
+        return dir;
+    }
+
+    public float fullFlightMove(PlayerMoveEvent event, double speed, boolean verticalSpeedMatch) {
+        if (PlayerUtils.isMoving()) {
+            double dir = getDir();
+
+            double xDir = Math.cos(Math.toRadians(dir + 90));
+            double zDir = Math.sin(Math.toRadians(dir + 90));
+
+            ((IVec3d) event.movement).setXZ(xDir * speed, zDir * speed);
+        } else {
+            ((IVec3d) event.movement).setXZ(0, 0);
+        }
+
+        float ySpeed = 0;
+
+        if (mc.options.jumpKey.isPressed())
+            ySpeed += speed;
+        if (mc.options.sneakKey.isPressed())
+            ySpeed -= speed;
+        ((IVec3d) event.movement).setY(verticalSpeedMatch ? ySpeed : ySpeed / 2);
+
+        return ySpeed;
+    }
+}
